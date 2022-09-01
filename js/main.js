3a58b0b6e96c554996a9c73afb065901493810a2
Ynsdz <yunusemredize@gmail.com>
2022-09-02 02:29:29 +0300

    style: removed add button instead add button, use enter key

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
 const todoBttn = document.getElementById("todoButton");
 let todoInput = document.getElementById("todoInput"); 
+const formfalan = document.getElementById('form');
 
-todoBttn.addEventListener('click', () => {
-    const todo = {
-        text : todoInput.value,
-        isCompleted : false,
-    }  
-  
-    const todoContainer = document.getElementById('todo-container')
-    let createTodo = document.createElement('li');
-    createTodo.textContent = todo.text; 
-    createTodo.classList.add('todo-text')
-    createTodo.setAttribute('isCompleted', todo.isCompleted);
-    todoContainer.appendChild(createTodo);
-    todoInput.value = '';
-    
-    createTodo.addEventListener('click',  (e)=>{
-        // let result = condition ? value1 : value2;
-        if (e.target.style.textDecoration == 'line-through') {
-            e.target.style.textDecoration = 'none';
-            e.target.style.opacity = 1;    
-        }else {
-            e.target.style.textDecoration = 'line-through';
-            e.target.style.opacity = 0.5;
-        }
-    } )
+todoInput.addEventListener('keypress', (event) => {
+    if ( event.code == "Enter") {
+        const todo = {
+            text : todoInput.value,
+            isCompleted : false,
+        }  
+        const todoContainer = document.getElementById('todo-container')
+        let createTodo = document.createElement('li');
+        createTodo.textContent = todo.text; 
+        createTodo.classList.add('todo-text')
+        createTodo.setAttribute('isCompleted', todo.isCompleted);
+        todoContainer.appendChild(createTodo);
+        todoInput.value = '';
+        
+        createTodo.addEventListener('click',  (e)=>{
+            if (e.target.style.textDecoration == 'line-through') {
+                e.target.style.textDecoration = 'none';
+                e.target.style.opacity = 1;    
+            }else {
+                e.target.style.textDecoration = 'line-through';
+                e.target.style.opacity = 0.5;
+            }
+        } )
+    }
 } )
