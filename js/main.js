f25b54eca0249d50261f7f8c7fb137700d37ba77
Ynsdz <yunusemredize@gmail.com>
2022-09-02 01:36:22 +0300

    style: improving readebilty
     enlarged app and centralized todo list and items, gived display flex for the all class which inside the main<app> tag

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 const todoBttn = document.getElementById("todoButton");
 let todoInput = document.getElementById("todoInput"); 
-// let listItems = document.getElementById('todo-list');
 
-todoBttn.addEventListener('click', ()=> {
-    const todo = 
-    {
+todoBttn.addEventListener('click', () => {
+    const todo = {
         text : todoInput.value,
         isCompleted : false,
     }  
@@ -12,37 +10,19 @@ todoBttn.addEventListener('click', ()=> {
     const todoContainer = document.getElementById('todo-container')
     let createTodo = document.createElement('li');
     createTodo.textContent = todo.text; 
-    createTodo.classList.add('todo-list')
+    createTodo.classList.add('todo-text')
     createTodo.setAttribute('isCompleted', todo.isCompleted);
-    todoContainer.appendChild(createTodo)
+    todoContainer.appendChild(createTodo);
     todoInput.value = '';
     
     createTodo.addEventListener('click',  (e)=>{
         // let result = condition ? value1 : value2;
-        if(e.target.style.textDecoration == 'line-through'){
+        if (e.target.style.textDecoration == 'line-through') {
             e.target.style.textDecoration = 'none';
-            e.target.style.opacity = 1;
-            
+            e.target.style.opacity = 1;    
         }else {
             e.target.style.textDecoration = 'line-through';
             e.target.style.opacity = 0.5;
         }
-
-    })
-})
-// setInterval()
-// const test = 
-// listItems.addEventListener('click', (e)=>{
-//     console.log(e.target)
-// })
-
-
-// let timerId = setTimeout(function tick() {
-//     let listItems = document.getElementsByClassName('todo-list');
-//     console.log(listItems)
-//     listItems.addEventListener('click', (e)=>{
-//     console.log(e.target)
-//     })
-//     timerId = setTimeout(tick, 150); // (*)
-//   }, 200);
-
+    } )
+} )
