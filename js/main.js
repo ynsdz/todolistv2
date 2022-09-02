0da4818b3b3b6652c7aed17efe4ae32539c1e94a
Ynsdz <yunusemredize@gmail.com>
2022-09-02 15:19:51 +0300

    build: can create button automatically when add todo, buttons will append automitcally into the todolist class, buttons not workin yet

--- js/main.js
+++ js/main.js
@@ -13,10 +13,28 @@ todoInput.addEventListener('keypress', (event) => {
         createTodo.textContent = todo.text; 
         createTodo.classList.add('todo-text')
         createTodo.setAttribute('isCompleted', todo.isCompleted);
-        todoContainer.appendChild(createTodo);
+
+        let container = document.getElementById('todo-container');
+        
+        let createList = document.createElement('div');
+        createList.classList.add('todo-list');
+
+        let createDelBtn = document.createElement('button');
+        let createEditBtn = document.createElement('button');
+        createDelBtn.textContent = 'Delete';
+        createEditBtn.textContent= 'Edit';
+        createDelBtn.classList.add('delete-btn');
+        createEditBtn.classList.add('edit-btn');
+        
+        createList.appendChild(createTodo);
+        createList.appendChild(createDelBtn);
+        createList.appendChild(createEditBtn);
+        container.appendChild(createList);
+        
         todoInput.value = '';
         
         createTodo.addEventListener('click',  (e)=>{
+            console.log(e);
             if (e.target.style.textDecoration == 'line-through') {
                 e.target.style.textDecoration = 'none';
                 e.target.style.opacity = 1;    
