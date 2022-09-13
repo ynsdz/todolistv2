fbb5f74fc16b284145f4f455ec5deb7f178fdb6a
Ynsdz <yunusemredize@gmail.com>
2022-09-13 20:33:33 +0300

    main: edit todos in localstorage

--- js/buttons.js
+++ js/buttons.js
@@ -14,7 +14,8 @@ const deleteFunc = (button) => {
 
 const editFunc = (button) => {
     button.addEventListener('click', function(){
-        todoStore.forEach(element => {
+        let todosArray = JSON.parse(localStorage.getItem('todos'));
+        todosArray.forEach(element => {
             if(element.id == button.parentElement.dataset.id){
                 if(button.textContent.toLowerCase()=='edit'){
                     button.parentElement.children[0].style.display = 'none';
@@ -23,12 +24,13 @@ const editFunc = (button) => {
                     button.parentElement.children[1].focus();
                     button.classList.add('save-btn');
                     button.textContent = 'Save';
-            
-                 }else{
+                    
+                }else{
                     button.parentElement.children[1].style.display = 'none';
                     button.parentElement.children[0].style.display = 'flex';
                     element.text = button.parentElement.children[1].value;
                     button.parentElement.children[0].innerHTML = element.text;
+                    localStorage.setItem('todos', JSON.stringify(todosArray));
                     button.classList.remove('save-btn');
                     button.textContent = 'Edit';
                 }
